97a1ebf27be955ab9b109883e02560f6cba1ada9
moussiomar90 <79989418+moussiomar90@users.noreply.github.com>
2021-08-07 16:55:05 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-simport React, { Component } from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,12 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
           <h1 className="App-title">DEV Branch testing fast  omar MouSSI2 TEST JENKINS</h1>
-=======
-          <h1 className="App-title">DEV Branch testing fast  omar MouSSI2 jenkins
-  </h1>
->>>>>>> a119e5db98b0f8e42c86f9f7170a3428d962bc33
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
